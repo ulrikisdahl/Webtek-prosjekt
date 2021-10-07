f379a8b9423a13d735d4c53ac704cbc5e68ace29
Jakob Opland <jakobop@stud.ntnu.no>
2021-10-07 20:27:26 +0200

    tekst

--- P3/script/footer&header.js
+++ P3/script/footer&header.js
@@ -5,3 +5,8 @@ let footerEl = document.createElement("footer");
 footerEl.classList.add("footer");
 bodyEl.appendChild(footerEl);
 
+/* Lager tekstelement */
+let madeByText = document.createElement("p");
+madeByText.innerHTML = "WEBSITE MADE BY....";
+footerEl.appendChild(madeByText);
+
