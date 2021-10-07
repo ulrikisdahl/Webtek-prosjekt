0a8541a1c146d17780b0e1d3d4617e4ed372204f
Jakob Opland <jakobop@stud.ntnu.no>
2021-10-07 20:11:27 +0200

    footer test

---
+++ P3/script/footer&header.js
@@ -0,0 +1,7 @@
+let bodyEl = document.querySelector("body");
+
+/* Lager footerelementet */
+let footerEl = document.createElement("footer");
+footerEl.classList.add("footer");
+bodyEl.appendChild(footerEl);
+
