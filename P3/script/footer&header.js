94a4ce9b2752d65f29d03757eb7946e8660e6c67
Sondre Alfnes <sondralf@stud.ntnu.no>
2021-10-16 18:44:31 +0200

    js

--- P3/script/footer&header.js
+++ P3/script/footer&header.js
@@ -1,12 +1,17 @@
 let bodyEl = document.querySelector("body");
+let script = document.querySelector("script"); 
 
 /* Lager footerelementet */
 let footerEl = document.createElement("footer");
-footerEl.classList.add("footer");
-bodyEl.appendChild(footerEl);
+bodyEl.insertBefore(footerEl, script);
 
 /* Lager tekstelement */
 let madeByText = document.createElement("p");
 madeByText.innerHTML = "WEBSITE MADE BY....";
 footerEl.appendChild(madeByText);
 
+// Lager header element
+let headerEl = document.createElement("header");
+
+ 
+bodyEl.insertBefore(headerEl, bodyEl.firstChild);
