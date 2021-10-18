679c93c52d1556434e8b2283e1d3c43290d0ac2f
Julian Ammouche Ottosen <julianao@stud.ntnu.no>
2021-10-18 19:14:11 +0200

    jobbet m age calc

---
+++ P3/script/age-calculator.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("age_calc_input");
+const months_input = document.querySelector("age_calc_months");
+const years_input = document.querySelector("age_calc_years");
+const select_input = document.querySelector("age_calc_select");
+const submit_button = document.querySelector("age_calc_submit");
+const output_p = document.querySelector("age_calc_output");
+    
