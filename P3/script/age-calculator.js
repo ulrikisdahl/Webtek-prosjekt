27d484c61652788a753d9938c31427d8299dc5cb
Julian <jotto0214@gmail.com>
2021-10-28 01:06:05 +0200

    jobbet m age-calc

--- P3/script/age-calculator.js
+++ P3/script/age-calculator.js
@@ -1,11 +1,28 @@
-const input = document.querySelector("age_calc_input");
-const months_input = document.querySelector("age_calc_months");
-const years_input = document.querySelector("age_calc_years");
-const select_input = document.querySelector("age_calc_select");
-const submit_button = document.querySelector("age_calc_submit");
-const output_p = document.querySelector("age_calc_output");
+const input = document.querySelector("#age_calc_input");
+const months_input = document.querySelector("#age_calc_months");
+const years_input = document.querySelector("#age_calc_years");
+const select_input = document.querySelector("#age_calc_select");
+const submit_button = document.querySelector("#age_calc_submit");
+const output_p = document.querySelector("#age_calc_output");
 
-months_input.addEventListener("click", function(){
-    //months_innput.classList.add("age_calc_selected");
-    //event.preventDefault();
-  });
+let age_type = "months";
+
+//months_input.onclick = button_function(months_input, years_input, "months");
+
+//years_input.onclick = button_function(years_input, months_input, "years");
+
+months_input.addEventListener("click", button_function(months_input, years_input, "months"));
+
+years_input.addEventListener("click", button_function(years_input, months_input, "years"));
+
+
+function button_function(add, remove, type, event){
+  add.classList.add("age_calc_selected");
+  remove.classList.remove("age_calc_selected");
+
+  age_type = type;
+
+  console.log(age_type);
+
+  event.preventDefault();
+}
