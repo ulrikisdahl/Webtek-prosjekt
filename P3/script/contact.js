073d37a29f26a1a04a0e771fc3d276e63ec50607
Julian Ammouche Ottosen <julianao@stud.ntnu.no>
2021-11-03 21:23:02 +0100

    m

--- P3/script/contact.js
+++ P3/script/contact.js
@@ -6,14 +6,12 @@ const contact_submit = document.querySelector("#contactSubmit");
 
 
 function contact_function(){
-    console.log("kjør");
   if(subject_input.value != "" && message_input.value != ""){
-    contact_submit.style.color = "red";
-    contact_submit.style.cursor = "pointer";
-    contact_submit.innerHTML = `<a href="mailto:contact@nala.com?&subject=${subject_input.value}&body=${message_input.value}" target="_blank">SEND MAIL</a>`;
+    contact_submit.style.color = "black";
+    contact_submit.innerHTML = `<a id="contact_link" href="mailto:contact@nala.com?&subject=${subject_input.value}&body=${message_input.value}" target="_blank">SEND MAIL</a>`;
   } else {
-    contact_submit.style.color = "grey";
-    contact_submit.style.cursor = "default";
+    contact_submit.style.color = "#A6A6A6";
+    contact_submit.style.color = "#A6A6A6";
     contact_submit.innerHTML = `SEND MAIL`;
   }
 }
